ca89240fdc0872b343091b51c27e211117213635
A107270277 <61675450+A107270277@users.noreply.github.com>
2021-01-11 00:42:38 +0800

    Add files via upload

---
+++ 機器人期末報告.py
@@ -0,0 +1,27 @@
+print ("哈囉～我是飲料機器人很開心為你服務")
+hobby = "由於設計者喜愛喝飲料所以主要為此設計的喔"
+print(hobby)
+
+name=input("你好,請問大名是?！:")
+print(name,"很高興認識你！")
+
+choose=input("想喝哪間飲料店呢？！:")
+print()
+
+print("你想喝可不可是嗎～我也很喜歡喝可不可！")
+
+blacktea_list=["熟成紅茶","胭脂紅茶","雪藏紅茶"]
+print("紅茶選項",blacktea_list)
+
+choose2=input("那你想要選擇哪種紅茶？:")
+print(blacktea_list[0])
+print(blacktea_list[0],":有茶味濃郁帶果香的")
+print(blacktea_liat[1],":有絲絨般的蜜桃果香")
+print(blacktea_list[2],":有冰淇淋與紅茶綿綿情意")
+
+
+
+pearl_list["白玉珍珠","黑玉珍珠"]
+print("珍珠選項",pearl_list)
+
+choose3=input("今天想要搭配哪種珍珠呢？")
